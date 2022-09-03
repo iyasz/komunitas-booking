7355d049666cd579839de96f8a753fa2deced024
Yasz Avellia <zakamaragames@gmail.com>
2022-09-03 09:35:08 +0700

    menambah include

---
+++ layouts/footer.php
@@ -0,0 +1,6 @@
+ 
+ <script src="<?= base_url() ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+
+    </html>
---
+++ layouts/header.php
@@ -0,0 +1,14 @@
+<?php 
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hotel Booking</title>
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/vendor/bootstrap/css/bootstrap.min.css">
+  </head>
+  <body>
+    <h1>header</h1>
