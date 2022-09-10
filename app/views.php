2f973821a565362f738e77719928601bf94c2d67
Yasz Avellia <zakamaragames@gmail.com>
2022-09-10 21:14:23 +0700

    menambah code in views.php

--- app/views.php
+++ app/views.php
@@ -6,6 +6,8 @@ if($page == 'login'){
     include "views/auth/login.php";
 }elseif($page == 'register'){
     include "views/auth/register.php";
+}elseif($page == 'beranda'){
+    include "views/page/home.php";
 }
 
 
