50a3d37354e68af1b0069c5127be7c30544a2080
Yasz Avellia <zakamaragames@gmail.com>
2022-09-11 17:06:43 +0700

    menambah title.php

--- layouts/header.php
+++ layouts/header.php
@@ -1,15 +1,4 @@
-<?php
 
-$page = isset($_GET['page']) ? $_GET['page'] : '';
-
-if($page == 'login'){
-    $title = "Login";
-}elseif($page == 'register'){
-    $title = "Register";
-}
-
-
-?>
 
 <!doctype html>
 <html lang="en">
