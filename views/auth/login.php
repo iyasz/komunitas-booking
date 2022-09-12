a22efcf2c2de98461cc2823db5978ef6f1b301bc
Yasz Avellia <zakamaragames@gmail.com>
2022-09-12 09:59:59 +0700

    mengubah style input

--- views/auth/login.php
+++ views/auth/login.php
@@ -6,33 +6,28 @@
                     <h2 class="fw-700 fs-2">Selamat Datang, <br>Travelfamily !</h2>
                     <h5 style="font-size: 14px;" class="fw-600 mb-4 mt-3 fs-text ">Petualangan baru telah menunggu <br>untuk kamu jelajahi</h5>
                     <form action="" method="post">
-                        <div class="mb-3">
+                        <div class="mb-3 ">
                             <div class="input-group">
-                                    <span class="input-group-text">
+                                    <span class="input-group-text border-0 bg-wh">
                                         <i class='bx bx-user'></i>
                                     </span>
-                                <input type="text" autocomplete="off" class="form-control" name="email" placeholder="Masukan Email" required>
+                                <input type="text" autocomplete="off" class="form-control bg-wh" name="email" placeholder="Masukan Email" required>
                             </div>
                         </div>
                         <div class="mb-3">
                             <div class="input-group">
-                                    <span class="input-group-text">
+                                    <span class="input-group-text border-0 bg-wh">
                                         <i class='bx bx-lock-alt'></i>
                                     </span>
-                                <input type="password" class="form-control" placeholder="Password" required name="password">
+                                <input type="password" class="form-control bg-wh" placeholder="Password" required name="password">
                             </div>
                         </div>
-
-                        <button type="submit" class="btn btn-primary mb-3">Daftar</button>
-
-                        <p class="text-center fs-sm">Don't have an account? <a class="text-decoration-none" href="index.php?page=register">Register</a></p>
-
-                        <div class="form-group">
-
+                        <div class="footer text-center">
+                            <button type="submit" class="btn btn-primary mt-4 w-100 mb-3 text-center">Masuk</button>
+                            
+                            <p class=" fs-sm">Don't have an account? <a class="text-decoration-none" href="index.php?page=register">Register</a></p>
                         </div>
-                        <div class="form-group">
 
-                        </div>
                     </form>
                 </div>
             </div>
