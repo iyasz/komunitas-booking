a3ccf9525ec74478790b6de8dede9c8648c83018
Yasz Avellia <zakamaragames@gmail.com>
2022-10-15 08:34:17 +0700

    menambah code php mailer

--- sendmail.php
+++ sendmail.php
@@ -1,5 +1,35 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
 require 'assets/vendor/PHPMailer/src/Exception.php';
 require 'assets/vendor/PHPMailer/src/PHPMailer.php';
 require 'assets/vendor/PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->Host = "smtp@gmail.com";
+$mail->SMTPAuth = true;
+$mail->Username = "zakamaragames@gmail.com";
+$mail->Password = "";
+$mail->SMTPSecure = "ssl";
+$mail->Port = 465;
+
+$mail->setFrom("noreply@treaveloka.com", "Traveloka");
+$mail->addAddress("zakamara@gmail.com");
+
+$mail->isHTML(true);
+
+$mail->Subject = "Verifikasi Email";
+$mail->Body = "<b>Hallo</b> Selamat Datang Di Traveloka!";
+
+$mail->send();
+
+if ($mail->send()) {
+    echo "Email Terkirim";
+} else {
+    echo "Mailer Error: $mail->ErrorInfo";
+}
